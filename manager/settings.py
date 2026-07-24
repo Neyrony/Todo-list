c9ad1340c86b49367b987dfcb9c847ef3ba99ffb
Neyrony <sserebriy@gmail.com>
2026-07-24 19:19:16 +0300

    feat: add django-crispy-forms

--- manager/settings.py
+++ manager/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
+    "crispy_forms",
+    "crispy_bootstrap4",
     "todo_list",
 ]
 
@@ -124,3 +126,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
