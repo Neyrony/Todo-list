b99053a3a313432e3dae6ddc1c3b5ca52829a62a
Neyrony <sserebriy@gmail.com>
2026-07-24 18:48:12 +0300

    feat: add logic for change status button in task list

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from todo_list.views import TaskListView, TagListView
+from todo_list.views import TaskListView, TagListView, TaskChangeStatusView
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="index"),
+    path("task/<int:pk>/change-status", TaskChangeStatusView.as_view(), name="task-change-status"),
     path("tags/", TagListView.as_view(), name="tags_list"),
 ]
 
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404, redirect
+from django.views import View
 from django.views.generic import ListView
 
 from todo_list.models import Task, Tag
@@ -9,6 +11,16 @@ class TaskListView(ListView):
 
     def get_queryset(self):
         return Task.objects.prefetch_related("tags")
+
+
+class TaskChangeStatusView(View):
+    def post(self, request, pk, **kwargs):
+        task = get_object_or_404(Task, pk=pk)
+
+        task.is_done = not task.is_done
+        task.save()
+
+        return redirect("todo_list:index")
 
 
 class TagListView(ListView):
