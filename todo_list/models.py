8a0de2c1e5af861a46c6eced09f6b0f8a1f59183
Neyrony <sserebriy@gmail.com>
2026-07-24 16:52:16 +0300

    feat: add __str__ method to Tag model

--- todo_list/models.py
+++ todo_list/models.py
@@ -9,5 +9,9 @@ class Task(models.Model):
     tags = models.ManyToManyField("Tag", related_name="tasks")
 
 
+
 class Tag(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
