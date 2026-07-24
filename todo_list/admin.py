ee7b2d78706116f21eb0d2e213151ffe4df79824
Neyrony <sserebriy@gmail.com>
2026-07-24 16:56:26 +0300

    feat: add Task and Tag in admin page

--- todo_list/admin.py
+++ todo_list/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from todo_list.models import Tag, Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("content", "date", "deadline", "is_done")
+    search_fields =  ("content",)
+    list_filter = ("deadline", "is_done")
+    list_per_page = 20
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    list_per_page = 20
+
+
+admin.site.unregister(Group)
