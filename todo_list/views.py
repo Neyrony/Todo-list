e7007a4456806de3028483f3a721d866c7b352ee
Neyrony <sserebriy@gmail.com>
2026-07-24 17:53:23 +0300

    fix: fix N + 1 problem in index page

--- todo_list/views.py
+++ todo_list/views.py
@@ -6,3 +6,6 @@ from todo_list.models import Task
 class TaskListView(ListView):
     model = Task
     paginate_by = 10
+
+    def get_queryset(self):
+        return Task.objects.prefetch_related("tags")
